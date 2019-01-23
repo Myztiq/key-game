f582f9a2c45956da0863bf2a83da873c0d8d1a36
Ryan Kahn <github@ryan-kahn.com>
2019-01-22 18:34:27 -0800

    Fixed post deck scan state.

--- App.js
+++ App.js
@@ -113,7 +113,7 @@ export default class App extends React.Component {
 
     const newState = {
       deckBeingScanned: false,
-      currentView: Views.MAIN
+      currentView: Views.REGISTER_GAME
     }
 
     if (deck) {
